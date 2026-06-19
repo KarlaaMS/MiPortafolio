e9eb40d0a89c3c1ff3276183bbd6e9e7b008abcd
KarlaaMS <salgadosanchez2000@gmail.com>
2026-06-18 19:15:49 -0500

    Segundo commit - Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
-import LogoPortafolio from '../assets/LogoPortafolio.svg'
-import '../index.css'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-function Navbar(){
-    return (
+function Navbar() {
+  const [abierto, setAbierto] = useState(false)
+
+  return (
     <nav>
-        <span><img src={LogoPortafolio} class="LogoPortafolio" alt="Portafolio"></img></span>
-    <ul>
-        <li><a href="#sobre-mi">Sobre mí</a></li>
-        <li><a href="#educacion">Educación</a></li>
-        <li><a href="#habilidades">Habilidades</a></li>
-        <li><a href="#proyectos">Proyectos</a></li>
-        <li><a href="#contacto">Contacto</a></li>
-    </ul>
-    </nav>    
-    )
+      <img src="../../public/LogoPortafolio.png" alt="Logo" className="LogoPortafolio" />
+
+      {/* Links escritorio */}
+      <ul className="nav-links">
+        <li><a href="/#sobre-mi" onClick={() => setAbierto(false)}>Sobre mí</a></li>
+        <li><a href="/#educacion" onClick={() => setAbierto(false)}>Educación</a></li>
+        <li><a href="/#habilidades" onClick={() => setAbierto(false)}>Habilidades</a></li>
+        <li><a href="/#proyectos" onClick={() => setAbierto(false)}>Proyectos</a></li>
+        <li><a href="/#contacto" onClick={() => setAbierto(false)}>Contacto</a></li>
+      </ul>
+
+      {/* Botón hamburguesa */}
+      <button
+        className="hamburguesa"
+        onClick={() => setAbierto(!abierto)}
+      >
+        {abierto ? '✕' : '☰'}
+      </button>
+
+      {/* Menú móvil */}
+      {abierto && (
+        <ul className="nav-mobile">
+          <li><a href="/#sobre-mi" onClick={() => setAbierto(false)}>Sobre mí</a></li>
+          <li><a href="/#educacion" onClick={() => setAbierto(false)}>Educación</a></li>
+          <li><a href="/#habilidades" onClick={() => setAbierto(false)}>Habilidades</a></li>
+          <li><a href="/#proyectos" onClick={() => setAbierto(false)}>Proyectos</a></li>
+          <li><a href="/#contacto" onClick={() => setAbierto(false)}>Contacto</a></li>
+        </ul>
+      )}
+    </nav>
+  )
 }
 
 export default Navbar
